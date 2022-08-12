9527072f640b662ff67bf4c17f0693e07a95c66a
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-12 14:28:49 -0700

    Add Request Header Parser Microservice

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// Request Header Parser Microservice
+app.get('/api/whoami', (req, res) => {
+  console.log(req.get('accept-language'))
+  console.log(req.get('user-agent'))
+  res.json({ipaddress: req.ip, language: req.get('accept-language'), software: req.get('user-agent')})
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
